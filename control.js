bdf09c61d1cec8bf7ccb073f5037c1f768604be5
dayoe <70726916+dayoe@users.noreply.github.com>
2021-07-17 22:19:09 -0700

    Add files via upload
    Server side javascript file to serve static html pages

---
+++ control.js
@@ -0,0 +1,36 @@
+var express = require('express');
+const path = require('path');
+
+var app = express();
+app.use(express.static('public'))
+
+app.set('port', 3000);
+
+app.get('/', function(req, res){
+	res.sendFile(path.join(__dirname + 'index.html'));
+});
+
+app.get('/employees', function(req, res){
+	res.sendFile(path.join(__dirname + '/employees.html'));
+});
+
+app.get('/other', function(req, res){
+	res.send('foo');
+});
+
+app.use(function(req,res){
+  res.type('text/plain');
+  res.status(404);
+  res.send('404 - Not Found');
+});
+
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.type('plain/text');
+  res.status(500);
+  res.send('500 - Server Error');
+});
+
+app.listen(app.get('port'), function(){
+  console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
+});
