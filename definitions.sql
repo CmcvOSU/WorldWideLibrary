46dbd7bf5fa5bf79b638f58ff7bab7a17803b77c
dayoe <70726916+dayoe@users.noreply.github.com>
2021-07-26 13:22:49 -0700

    Added Patron_book sample data. LOCK and UNLOCK tables for insert v1.1

--- definitions.sql
+++ definitions.sql
@@ -46,25 +46,42 @@ CREATE TABLE `Books` (
 	`genre` INT(11),
 	FOREIGN KEY (`genre`) REFERENCES Genres(id)
 	) ENGINE = InnoDB;
+	  
+LOCK TABLES `Genres` WRITE;
+INSERT INTO Genres (name) VALUES ("Fiction");
+INSERT INTO Genres (name) VALUES ("Horror");
+INSERT INTO Genres (name) VALUES ("American History");
+UNLOCK TABLES;
 
-INSERT INTO `Patrons` VALUES (1, 'Daniel', 'Park', NULL), (2, 'Muhammad', 'Lee', NULL), 
-(3, 'Michael', 'Calmis', NULL), (4, 'Mason', 'Sperling', NULL), (5, 'Emily', 'Koch', NULL), 
-(6, 'Cordelia', 'Watson', NULL), (7, 'Danny', 'Padilla', NULL);
-
+LOCK TABLES `Books` WRITE;
 INSERT INTO `Books` VALUES ('9780446310789', 'To Kill a Mockingbird', 'Harper Lee', 1), 
 ('19849780140862539', '1984', 'George Orwell', 1), 
 ('9781405879910', 'The Great Gatsby', 'F. Scott Fitzgerald', 3), 
 ('9783839893876', 'Pride and Prejudice', 'Jane Austen', 2), 
-('9780399529207', 'Lord of the Flies', 'William Golding', 3);    
+('9780399529207', 'Lord of the Flies', 'William Golding', 3);  
+UNLOCK TABLES;
 
-INSERT INTO Genres (name) VALUES ("Fiction");
-INSERT INTO Genres (name) VALUES ("Horror");
-INSERT INTO Genres (name) VALUES ("American History");
-
+LOCK TABLES `Librarians` WRITE;
 INSERT INTO Librarians (first_name, last_name, focus) VALUES ("Benjamin", "Murphy", 2);
 INSERT INTO Librarians (first_name, focus) VALUES ("Jack", 1);
 INSERT INTO Librarians (first_name, last_name) VALUES ("Jessica", "Barn");
+UNLOCK TABLES;
 
+LOCK TABLES `Rooms` WRITE;
 INSERT INTO Rooms (roomNumber, capacity) VALUES (102, 50);
 INSERT INTO Rooms (roomNumber, capacity) VALUES (100, 10);
 INSERT INTO Rooms (roomNumber, capacity) VALUES (101, 2);
+UNLOCK TABLES;
+
+LOCK TABLES `Patrons` WRITE;
+INSERT INTO `Patrons` VALUES (1, 'Daniel', 'Park', NULL), (2, 'Muhammad', 'Lee', NULL), 
+(3, 'Michael', 'Calmis', NULL), (4, 'Mason', 'Sperling', NULL), (5, 'Emily', 'Koch', NULL), 
+(6, 'Cordelia', 'Watson', NULL), (7, 'Danny', 'Padilla', NULL);
+UNLOCK TABLES;
+
+LOCK TABLES `Patron_book` WRITE;
+INSERT INTO `Patron_book` VALUES (1, '9780399529207', '2020-12-05', '2020-12-20'),
+(5, '19849780140862539', '2021-2-12', '2021-2-19'),
+(3, '9781405879910', '2021-2-08', '2022-2-20'),
+(7, '9780399529207', '2020-12-05', '2020-12-20');
+UNLOCK TABLES;
