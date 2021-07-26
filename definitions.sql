4074352e36eccd522613e1bb2869a92d0e445137
dayoe <70726916+dayoe@users.noreply.github.com>
2021-07-26 11:54:23 -0700

    Append creation of tables Patrons, Patron_book, and Books v1.0
    Also inserted sample data. Note, Books(isbn) data type changed to VARCHAR b/c max INT value is lower than isbn.

--- definitions.sql
+++ definitions.sql
@@ -19,6 +19,44 @@ CREATE TABLE Rooms (
     PRIMARY KEY (roomNumber)
 );
 
+DROP TABLE IF EXISTS `Patrons`;
+CREATE TABLE `Patrons` (
+	`libraryID` INT(11) PRIMARY KEY AUTO_INCREMENT,
+	`firstName` VARCHAR(255),
+	`lastName` VARCHAR(255),
+	`reservation` INT(11),
+	FOREIGN KEY (`reservation`) REFERENCES Rooms(`roomNumber`)
+	) ENGINE = InnoDB;
+
+DROP TABLE IF EXISTS `Patron_book`;
+CREATE TABLE `Patron_book` (
+	`pid` INT(11),
+	`bid` VARCHAR(255),
+	`checkoutDate` DATE,
+	`returnDate` DATE,
+	FOREIGN KEY (`pid`) REFERENCES Patrons(libraryID),
+	FOREIGN KEY (`bid`) REFERENCES Books(isbn)
+	) ENGINE = InnoDB;
+    
+DROP TABLE IF EXISTS `Books`;
+CREATE TABLE `Books` (
+	`isbn` VARCHAR(255) PRIMARY KEY,
+	`title` VARCHAR(255),
+	`author` VARCHAR(255),
+	`genre` INT(11),
+	FOREIGN KEY (`genre`) REFERENCES Genres(id)
+	) ENGINE = InnoDB;
+
+INSERT INTO `Patrons` VALUES (1, 'Daniel', 'Park', NULL), (2, 'Muhammad', 'Lee', NULL), 
+(3, 'Michael', 'Calmis', NULL), (4, 'Mason', 'Sperling', NULL), (5, 'Emily', 'Koch', NULL), 
+(6, 'Cordelia', 'Watson', NULL), (7, 'Danny', 'Padilla', NULL);
+
+INSERT INTO `Books` VALUES ('9780446310789', 'To Kill a Mockingbird', 'Harper Lee', 1), 
+('19849780140862539', '1984', 'George Orwell', 1), 
+('9781405879910', 'The Great Gatsby', 'F. Scott Fitzgerald', 3), 
+('9783839893876', 'Pride and Prejudice', 'Jane Austen', 2), 
+('9780399529207', 'Lord of the Flies', 'William Golding', 3);    
+
 INSERT INTO Genres (name) VALUES ("Fiction");
 INSERT INTO Genres (name) VALUES ("Horror");
 INSERT INTO Genres (name) VALUES ("American History");
